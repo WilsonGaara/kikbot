fedb2681dd41f4635ff892f7e16c52868c41cf47
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-03-08 19:25:56 -0300

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -6,8 +6,8 @@ const moment = require('moment');
 moment.locale('pt-BR'); 
 
          const embed = new Discord.RichEmbed()
-		 .setAuthor(`Meus status`, message.author.avatarURL)
-         .setDescription('Uso de memória RAM: ' + `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB` + '\nUso de Processador(CPU): ' + `${(process.cpuUsage().user / 1024 / 1024).toFixed(2)}` + '%')
+		 .setAuthor(`Meus status`, client.user.avatarURL)
+         .setDescription('Uso de memória RAM: ' + `**${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB**` + '\nUso de Processador(CPU): ' + `**${(process.cpuUsage().user / 1024 / 1024).toFixed(2)}**` + '**%**')
         .setColor(`GOLD`)
         .setFooter(`© KikBot 2018 l Informação - ` + `${moment().format('LLLL')}`, client.user.avatarURL)
          
